3b951b19f1fc9dc898154355b9bc3caea15c5ac7
Samuel Bazaga <samuelbazaga@gmail.com>
2022-10-13 17:04:37 +0200

    chore: Add module information

--- packages/injection/index.ts
+++ packages/injection/index.ts
@@ -1,5 +1,12 @@
 import 'reflect-metadata';
 
+/**
+ * Dependency Injection Module for Xeito Framework (https://github.com/aerotoad/xeito)
+ * Simplified implementation based on TypeDI (https://github.com/typestack/typedi)
+ * Licensed under MIT License
+ */
+
+
 // Check if Reflect metadata is available
 if (typeof Reflect === 'undefined' || typeof Reflect.getMetadata === 'undefined') {
   throw new Error('reflect-metadata shim is required when using class decorators');
