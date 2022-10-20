f9ad25c11d3114682f426090b38c741016a5fb7c
Samuel Bazaga <samuelbazaga@gmail.com>
2022-10-20 16:39:28 +0200

    fix(router): Missing export for RouterLink

--- packages/router/index.ts
+++ packages/router/index.ts
@@ -8,6 +8,9 @@ export { Router } from "./components/router";
 // Export RouterView component
 export { RouterView } from "./components/router-view";
 
+// Export RouterLink component
+export { RouterLink } from "./components/router-link";
+
 // Export interfaces
 export type { Route } from "./interfaces/route";
 export type { RouterOptions } from "./interfaces/router-options";
