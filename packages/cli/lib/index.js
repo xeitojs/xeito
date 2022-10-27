c902f9433210b6bfeab63ff3057745c1ebfaf087
Samuel Bazaga <samuelbazaga@gmail.com>
2022-10-27 19:23:21 +0200

    feat(): Show error if running g in a non-xeito folder

--- packages/cli/lib/index.js
+++ packages/cli/lib/index.js
@@ -2,6 +2,8 @@ import { Command } from 'commander';
 import fs from 'fs';
 import { createProject } from './create-project.js';
 import { generateFiles } from './generate.js';
+import chalk from 'chalk';
+import emoji from 'node-emoji';
 
 export function initialize() {
 
@@ -27,7 +29,21 @@ export function initialize() {
     .alias('g')
     .description('Generate a new component or service')
     .action((type, name) => {
-      generateFiles(type, name);
+      // Get the current working directory
+      const cwd = process.cwd();
+
+      // Check if the current directory is a xeito project
+      if (fs.existsSync(`${cwd}/.xeitorc`)) {
+        generateFiles(type, name);
+      } else {
+        console.log(
+          emoji.emojify(':x: -'), 
+          chalk.red('Sorry!'), 
+          chalk.yellow('xeito g'), 
+          chalk.red('can only be run inside a xeito project directory')
+        );  
+      }
+      
     });
   
 
