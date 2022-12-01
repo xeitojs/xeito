c39816ca570e140c3440cfcc217c72bef985dd12
Samuel Bazaga <samuelbazaga@gmail.com>
2022-12-01 20:45:47 +0100

    refactor: Add comments

--- packages/core/decorators/state.ts
+++ packages/core/decorators/state.ts
@@ -1,12 +1,24 @@
+
+/**
+ * State decorator
+ * Wraps the state property with a Proxy or a getter/setter
+ * This way we can detect when the state changes and trigger a re-render
+ */
 export function State() {
   
   return function _StateDecorator(target: any, key: string) {
     
+    /**
+     * Define the property in the target object
+     */
     Object.defineProperty(target, key, {
       get: function() {
+        // Return the state
         return this._XeitoInternals.state[key];
       },
       set: function(value) {
+
+        // If the value is an object, wrap it with a Proxy
         if (typeof value === 'object') {
           const proxy = new Proxy(value, {
             deleteProperty: () => {
@@ -22,6 +34,7 @@ export function State() {
           })
           this._XeitoInternals.state[key] = proxy;
         } else {
+          // If the value is not an object, just set it and trigger a re-render
           this._XeitoInternals.state[key] = value;
           this.requestUpdate();
         }
