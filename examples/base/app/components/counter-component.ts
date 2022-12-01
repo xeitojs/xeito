aa2a64ad51d35d80d55433de83a90d143acba88f
Samuel Bazaga <samuelbazaga@gmail.com>
2022-12-01 21:32:27 +0100

    feat: Update examples

--- examples/base/app/components/counter-component.ts
+++ examples/base/app/components/counter-component.ts
@@ -1,12 +1,20 @@
 import { Component, html, State } from "../../../../packages/core";
 import { XeitoComponent } from "../../../../packages/core/classes/xeito-component";
+import { Inject } from "../../../../packages/injection/dist";
+import { GreetService } from "../services/greet-service";
 
 @Component({
   selector: 'app-counter'
 })
 export class CounterComponent extends XeitoComponent {
 
+  @Inject() greetService: GreetService;
+
   @State() count: number = 0;
+
+  constructor() {
+    super();
+  }
 
   increment() {
     this.count++;
