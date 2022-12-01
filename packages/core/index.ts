f6aa4b92899e138084fcffe4265e82a84e5f62df
Samuel Bazaga <samuelbazaga@gmail.com>
2022-12-01 20:40:39 +0100

    fix: Update exports

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,22 +1,19 @@
 import { createElement } from "./functions/create-element";
 import { digestUpdate } from "./functions/digest-update";
-import { render } from "./functions/render";
 
-// Export core module
-export const Xeito = {
-  createElement: createElement,
-  render: render,
-  digestUpdate: digestUpdate
-}
+import { render, html, svg, Hole, Renderable, TemplateFunction } from 'uhtml';
 
 // Export core decorators
 export { Component } from "./decorators/component";
 export { State } from "./decorators/state";
-export { Ref } from "./decorators/ref";
 
-// Export types
-export type { VNode } from "snabbdom";
-export type { ComponentData } from "./interfaces/component-data";
-
-// Export classes
-export { Emitter } from "./classes/emitter";
+/**
+ * Re-export uhtml
+ * Allows the user to use uhtml directly from the xeito module
+ * uhtml is a library to build declarative and reactive UI via template literals tags.
+ * {@link https://github.com/WebReflection/uhtml}
+ * Xeito uses uhtml to render the component template and update the DOM
+ * Created by @WebReflection All credits to him
+ */
+export { render, html, svg, Hole };
+export type { Renderable, TemplateFunction };
