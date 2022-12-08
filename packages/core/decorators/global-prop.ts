a70f0f2bd0ce6979374680132f9ca87dd449019d
Samuel Bazaga <samuelbazaga@gmail.com>
2022-12-08 17:44:24 +0100

    feat(): Add decorator to access global properties

---
+++ packages/core/decorators/global-prop.ts
@@ -0,0 +1,25 @@
+
+export function GlobalProp() {
+
+  return function _GlobalPropDecorator(target: any, key: string) {
+
+    Object.defineProperty(target, key, {
+      get: function() {
+        if (!this.global) {
+          console.warn(`
+            Cannot get the value of the global property '${key}' from component <${this._XeitoInternals.selector}>.
+            The global object is not attached to the component.
+            Did you try to get the value of the global property from the constructor?
+            If so, you should get the value from the onCreate() hook instead.
+          `)
+        }
+        return this.global?.properties[key];
+      },
+      set: function(value) {
+        console.warn(`Cannot set the value of the global property '${key}' from a component.`);
+      }
+    });
+
+  }
+
+}
