233e6d340ff420fd4e6582e156bfc315935c02c3
Samuel Bazaga <samuelbazaga@gmail.com>
2022-12-11 14:35:38 +0100

    feat(core): Update exports

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -4,19 +4,29 @@ import { render, html, svg, Hole, Renderable, TemplateFunction } from 'uhtml';
 // Export Xeito Application Class
 export { Xeito } from './classes/xeito';
 
+// Export the Xeito Component Class
+export { XeitoComponent } from './classes/xeito-component';
+
+// Export the Xeito Plugin Class
+export { XeitoPlugin } from './classes/xeito-plugin';
+
 // Export core decorators
+export { Action } from './decorators/action';
 export { Component } from './decorators/component';
-export { State } from './decorators/state';
-export { Prop } from './decorators/prop';
 export { Event } from './decorators/event';
+export { Global } from './decorators/global';
+export { Prop } from './decorators/prop';
 export { Ref } from './decorators/ref';
+export { State } from './decorators/state';
 
 // Export core interfaces
+export type { ActionMetadata } from './interfaces/action-metadata';
+export type { ActionResult } from './interfaces/action-result';
+export type { AttributeChanges } from './interfaces/attribute-changes';
 export type { ComponentMetadata } from './interfaces/component-metadata';
 export type { ElementRef } from './interfaces/element-ref';
-export type { AttributeChanges } from './interfaces/attribute-changes';
-export type { ActionResult } from './interfaces/action-result';
 export type { EventConfig } from './interfaces/event-config';
+export type { XeitoGlobal } from './interfaces/xeito-global';
 
 /**
  * Re-export uhtml
@@ -24,7 +34,7 @@ export type { EventConfig } from './interfaces/event-config';
  * uhtml is a library to build declarative and reactive UI via template literals tags.
  * {@link https://github.com/WebReflection/uhtml}
  * Xeito uses uhtml to render the component template and update the DOM
- * Created by @WebReflection All credits to him
+ * Created by @WebReflection - All credits to them
  */
 export { render, html, svg, Hole };
 export type { Renderable, TemplateFunction };
