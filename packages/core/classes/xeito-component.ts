9bc5506eb50f28ac924aec96aa2f2af7c71c6695
Samuel Bazaga <samuelbazaga@gmail.com>
2022-12-15 10:18:56 +0100

    feat(): Move logic away from decorator and separate state and props

--- packages/core/classes/xeito-component.ts
+++ packages/core/classes/xeito-component.ts
@@ -1,104 +1,247 @@
 import { AttributeChanges } from '../interfaces/attribute-changes';
 import { ActionResult } from '../interfaces/action-result';
-import { Hole } from 'uhtml';
+import { Hole, render, Renderable } from 'uhtml';
 
 export class XeitoComponent extends HTMLElement {
-
+  
   /**
-   * Global properties object (will be populated by the parent component or the Xeito instance)
-   */
+  * Xeito internals object
+  * Will be populated by the @Component decorator 
+  * with the component's metadata
+  */
+  private _XeitoInternals: Record<string, any> = {};
+  
+  private _DOMRoot: HTMLElement | ShadowRoot;
+  private _template: Node | Hole | Renderable;
+  private _state: Record<string, any> = {};
+  private _props: Record<string, any> = {};
+  
+  /**
+  * Global properties object (will be populated by the parent component or the Xeito instance)
+  */
   global: Record<string, any>;
+  
+  /**
+  * Slot Content
+  * Will be populated by the constructor
+  * It will contain the slotted content of the component
+  * eg: <xeito-component><div slot="header">Header</div></xeito-component>
+  * slotContent.header will contain the div element
+  * This can be accessed inside the render method
+  * eg: html`<div>${this.slotContent.header}</div>`
+  */
+  slotContent: Record<string, any> = {};
+  
+  constructor() {
+    super();
+    
+    // Set the default _XeitoInternals object as received from the decorator
+    this._XeitoInternals = Object.assign({}, this.constructor.prototype._XeitoInternals);
+    
+    // Set the slotted content
+    this.slotContent = this.getSlotContent();
+    
+    // Set the global property
+    this.global = this._XeitoInternals.global;
+    
+    // Assign the children global
+    this.assignChildrenGlobal();
+    
+    // Bind the methods to the class instance
+    this.bindMethods();
+    
+    /** 
+    * Set the root element to render the template in
+    * If it's a shadow root, create a shadow root
+    * If it's not a shadow root, use the element itself (default)
+    */
+    let DOMRoot: HTMLElement | ShadowRoot = this;
+    if (this._XeitoInternals.shadow === true) {
+      this.attachShadow({ mode: 'open' });
+      DOMRoot = this.shadowRoot as ShadowRoot;
+    }
+    // Set the DOMRoot in the _XeitoInternals
+    this._DOMRoot = DOMRoot;
+    
+    // Render the template for the first time
+    this._template = render(this._DOMRoot, this.render() as Renderable);
+    
+    // Add the styles to the DOM
+    if (this._XeitoInternals.styles) {
+      const style = document.createElement('style');
+      style.textContent = this._XeitoInternals.styles;
+      this._DOMRoot.appendChild(style);
+    }
+    
+    this.onWillCreate();
+  }
+  
+  /**
+  * Get the slotted content of the component
+  * @returns { Record<string, any> } Slot content object
+  */
+  private getSlotContent() {
+    const slotContent: Record<string, any> = {};
+    const children = Array.from(this.children);
+    
+    if (children) {
+      for(let child in children) {
+        const slot = children[child].getAttribute('slot');
+        if (slot) {
+          if (!slotContent[slot]) slotContent[slot] = [];
+          slotContent[slot].push(children[child]);
+        } else {
+          if (!slotContent.default) slotContent.default = [];
+          slotContent.default.push(children[child]);
+        }
+      }
+    }
+    
+    return slotContent;
+  }
+  
+  /**
+  * Assign children global
+  * Assigns the global property to the children of the component
+  */
+  private assignChildrenGlobal() {
+    this._XeitoInternals.children?.forEach((child: typeof XeitoComponent) => {
+      child.prototype._XeitoInternals.global = this.global;
+    });
+  }
+  
+  /**
+  * Bind methods
+  * Binds the methods of the component to the component itself
+  * This is done to avoid having to wrap the methods in arrow functions
+  * eg: onClick={this.onClick} instead of onClick={() => this.onClick()}
+  * NOTE: Arrow functions are still required to pass data to the method or native events
+  * eg: onClick={() => this.onClick(data)} or onClick={(e) => this.onClick(e)}
+  */
+  private bindMethods() {
+    const methods = Object.getOwnPropertyNames(this.constructor.prototype);
+    methods.forEach((method: string) => {
+      if (method !== 'constructor' && method !== 'render' && typeof this[method] === 'function') {
+        this[method] = this[method].bind(this);
+      }
+    });
+  }
 
   /**
-   * Render method desgin to be overriden by the user
-   */
-  render(): Hole | void {}
+  * Request an update of the component
+  */
+  requestUpdate() {
+    if (this._template) {
+      // Use requestAnimationFrame to avoid multiple updates in the same frame
+      window.requestAnimationFrame(() => {
+        this._template = render(this._DOMRoot, this.render() as Renderable);
+      });
+    }
+  }
+
+  setState(key: string, value: any, triggerUpdate: boolean = true) {
+    this._state[key] = value;
+    if (triggerUpdate) this.requestUpdate();
+  }
+
+  getState(key: string) {
+    return this._state[key];
+  }
 
   /**
-   * On mount method desgined to be overriden by the user
-   * Gets called when the component is mounted (connectedCallback)
-   */
-  onCreate() {}
-
+  * Use method (Use an action inside the component)
+  * The 'use' method is used to provide actions to the template
+  * Actions have to be provided in the Component decorator options
+  * eg: @Component({ actions: [Action1, Action2] })
+  * @param selector 
+  * @param args 
+  * @returns 
+  */
+  use(selector: string, ...args: any[]): ActionResult | void {
+    return (e: HTMLElement) => {
+      // Check if the selector is a local action
+      let action = this._XeitoInternals.actions.find((action: any) => action.selector === selector);
+      if (!action) {
+        // Check if the selector is a global action
+        action = this.global.actions.find((action: any) => action.selector === selector);
+      }
+      
+      if (action) {
+        return new action(e.parentElement, ...args);
+      } else {
+        throw new Error(`Action '${selector}' not found in component '<${this._XeitoInternals.selector}>', did you forget to add it to the actions array or install the plugin?`);
+      }
+    }
+  }
+  
   /**
-   * On destroy method desgined to be overriden by the user
-   * Gets called when the component is destroyed (disconnectedCallback)
-   */
-  onDestroy() {}
-
-  /**
-   * On changes method desgin to be overriden by the user
-   * Gets called when an attribute changes (attributeChangedCallback)
-   * @param { AttributeChanges } changes Attribute changes object
-   */
-  onChanges(changes: AttributeChanges) {}
-
-  /**
-   * Native attributeChangedCallback
-   * Calls the onChanges method and requests an update of the component
-   * @param name 
-   * @param oldValue 
-   * @param newValue 
-   */
+  * Native attributeChangedCallback
+  * Calls the onChanges method and requests an update of the component
+  * @param name 
+  * @param oldValue 
+  * @param newValue 
+  */
   attributeChangedCallback(this: any, name: string, oldValue: string, newValue: string) {
-
+    
     const type = typeof this[name];
     
     switch(type) {
       case 'number':
-        oldValue = Number(oldValue) as any;
-        newValue = Number(newValue) as any;
-        break;
+      oldValue = Number(oldValue) as any;
+      newValue = Number(newValue) as any;
+      break;
       case 'boolean':
-        oldValue = oldValue === 'true' ? true : false as any;
-        newValue = newValue === 'true' ? true : false as any;
-        break;
+      oldValue = oldValue === 'true' ? true : false as any;
+      newValue = newValue === 'true' ? true : false as any;
+      break;
     }
-
+    
     const changes: AttributeChanges = { name, oldValue, newValue };
     this.onChanges(changes);
     this.requestUpdate();
   }
-
+  
   /**
-   * Native connectedCallback
-   * Calls the onMount method
-   */
+  * Native connectedCallback
+  * Calls the onMount method
+  */
   connectedCallback() {
     this.onCreate();
   }
-
+  
   /**
-   * Native disconnectedCallback
-   * Calls the onDestroy method
-   */
+  * Native disconnectedCallback
+  * Calls the onDestroy method
+  */
   disconnectedCallback() {
     this.onDestroy();
   }
-
+  
   /**
-   * Request an update of the component
-   * Designed to be overriden by the @Component decorator at runtime
-   */
-  requestUpdate() {}
-
+  * Render method desgin to be overriden by the user
+  */
+  render(): Hole | void {}
+  
+  onWillCreate() {}
+  
   /**
-   * Use an action inside the component
-   * Designed to be overriden by the @Component decorator at runtime
-   * @param selector 
-   * @param args 
-   */
-  use(selector: string, ...args: any[]): ActionResult | void {}
-
+  * On mount method desgined to be overriden by the user
+  * Gets called when the component is mounted (connectedCallback)
+  */
+  onCreate() {}
+  
   /**
-   * Slot Content
-   * It will be populated by the @Component decorator at runtime
-   * It will contain the slotted content of the component
-   * eg: <xeito-component><div slot="header">Header</div></xeito-component>
-   * slotContent.header will contain the div element
-   * This can be accessed inside the render method 
-   * eg: html`<div>${this.slotContent.header}</div>`
-   */
-  slotContent: Record<string, any> = {};
+  * On destroy method desgined to be overriden by the user
+  * Gets called when the component is destroyed (disconnectedCallback)
+  */
+  onDestroy() {}
+  
+  /**
+  * On changes method desgin to be overriden by the user
+  * Gets called when an attribute changes (attributeChangedCallback)
+  * @param { AttributeChanges } changes Attribute changes object
+  */
+  onChanges(changes: AttributeChanges) {}
+  
   
 }
--- packages/core/decorators/component.ts
+++ packages/core/decorators/component.ts
@@ -4,30 +4,7 @@ import { XeitoComponent } from './../classes/xeito-component';
 
 export function Component(componentMetadata: ComponentMetadata) {
 
-  return function ComponentDecorator<T extends { new (...args: any[]): {} }>(constructor: T) {
-    /**
-     * Bind all methods to the class instance
-     * This allows to use the methods in the template without the need to bind them or use arrow functions
-     * NOTE: Arrow functions are still required to pass data to the method
-     * eg: 
-     *  @click=${this.method} method will receive the event as parameter
-     *  @click=${(e: CustomEvent) => this.method(e, 'data')} method will receive the event and the data as parameters
-     */
-    let keys = Reflect.ownKeys(constructor.prototype);
-
-    keys.forEach((key: string | symbol) => {
-      // Skip constructor
-      if (key === 'constructor') return;
-      if (key === 'render') return;
-
-      const descriptor = Object.getOwnPropertyDescriptor(constructor.prototype, key);
-      
-      // Only bind methods
-      if (typeof descriptor?.value === 'function') {
-        Object.defineProperty(constructor.prototype, key, bindMethod(constructor, key, descriptor));
-      }
-
-    });
+  return function ComponentDecorator(constructor: any) {
 
     /**
      * Handle component-specific styles
@@ -41,184 +18,29 @@ export function Component(componentMetadata: ComponentMetadata) {
      * Add the default _XeitoInternals object to the class
      */
     constructor.prototype._XeitoInternals = {
-      DOMRoot: null,
       selector: componentMetadata.selector,
+      actions: componentMetadata.actions || [],
+      imports: componentMetadata.imports || [],
+      shadow: componentMetadata.shadow || false,
+      DOMRoot: null,
       styles: componentStyles,
       template: null,
-      actions: componentMetadata.actions || [],
       state: {},
       props: {},
       global: null
-    }
-
-    // Extend the XeitoComponent class for the ComponentClass to inherit the methods
-    interface ComponentClass extends XeitoComponent {}
-
-    // Create a new ComponentClass that extends the original class
-    class ComponentClass extends constructor {
-      
-      // Declare the _XeitoInternals property
-      public _XeitoInternals;
-
-      // Declare the componentChildren property
-      public slotContent: Record<string, Element[]> = {
-        default: []
-      };
-
-      constructor(...args: any[]) {
-        super(...args);
-
-        // Set the SlottedContent
-        const children = Array.from(this.children);
-        if (children) {
-          for(let child in children) {
-            const slot = children[child].getAttribute('slot');
-            if (slot) {
-              if (!this.slotContent[slot]) {
-                this.slotContent[slot] = [];
-              }
-              this.slotContent[slot].push(children[child]);
-            } else {
-              this.slotContent.default.push(children[child]);
-            }
-          }
-        }
-
-        // Set the _XeitoInternals from the constructor
-        this._XeitoInternals = constructor.prototype._XeitoInternals;
-        
-        // Set the global property on the component
-        this.global = this._XeitoInternals.global;
-
-        /**
-         * Add the global property to the component
-         */
-        componentMetadata.imports?.forEach((component: any) => {
-          component.prototype._XeitoInternals.global = this.global;
-        });
-
-        // Check if the component extends XeitoElement
-        if (!this.attachShadow) {
-          throw new Error(`Invalid component, did you forget to extend XeitoElement in component '<${this._XeitoInternals.selector}>'?`);
-        }
-
-        /** 
-         * Set the root element to render the template in
-         * If it's a shadow root, create a shadow root
-         * If it's not a shadow root, use the element itself (default)
-         */
-        let DOMRoot: HTMLElement | ShadowRoot = this;
-        if (componentMetadata.shadow === true) {
-          this.attachShadow({ mode: 'open' });
-          DOMRoot = this.shadowRoot as ShadowRoot;
-        }
-        // Set the DOMRoot in the _XeitoInternals
-        this._XeitoInternals.DOMRoot = DOMRoot;
-
-        // Render the template for the first time
-        this._XeitoInternals.template = render(this._XeitoInternals.DOMRoot, this.render() as Renderable);
-
-        // Add the styles to the DOM
-        if (this._XeitoInternals.styles) {
-          const style = document.createElement('style');
-          style.textContent = this._XeitoInternals.styles;
-          this._XeitoInternals.DOMRoot.appendChild(style);
-        }
-      }
-
-      requestUpdate() {
-        if (this._XeitoInternals.template) {
-          this._XeitoInternals.template = render(this._XeitoInternals.DOMRoot, this.render() as Renderable);
-        }
-      }
-
-      /**
-       * Override the 'use' method from the XeitoComponent class
-       * The 'use' method is used to provide actions to the template
-       * Actions have to be provided in the Component decorator options
-       * eg: @Component({ actions: [Action1, Action2] })
-       * @param selector 
-       * @param args 
-       * @returns 
-       */
-      use(selector: string, ...args: any[]) {
-        return (e: HTMLElement) => {
-          // Check if the selector is a local action
-          let action = this._XeitoInternals.actions.find((action: any) => action.selector === selector);
-          if (!action) {
-            // Check if the selector is a global action
-            action = this.global.actions.find((action: any) => action.selector === selector);
-          }
-          
-          if (action) {
-            return new action(e.parentElement, ...args);
-          } else {
-            throw new Error(`Action '${selector}' not found in component '<${this._XeitoInternals.selector}>', did you forget to add it to the actions array or install the plugin?`);
-          }
-        }
-      }
     }
 
     /**
      * Register the component in the customElements registry
      */
     if (!customElements.get(componentMetadata.selector)) {
-      customElements.define(componentMetadata.selector, ComponentClass);
+      customElements.define(componentMetadata.selector, constructor);
     } else {
       console.warn(`Component '${componentMetadata.selector}' already registered`);
     }
     
     // Return the new ComponentClass
-    return ComponentClass;
+    return constructor;
   }
 
-}
-
-/**
- * Genreates a new descriptor for the property that will bind the method to the class instance
- * 
- * Based on auto-bind by andreypopp {@link https://github.com/andreypopp/autobind-decorator}
- * All credit goes to him
- * 
- * @param target The class that contains the property
- * @param key The name of the property
- * @param descriptor The property descriptor
- * @returns New property descriptor
- */
-function bindMethod(target: any, key: string | symbol, descriptor: Record<any, any>): Record<any, any> {
-	let _fn = descriptor.value;
-
-	// In IE11 calling Object.defineProperty has a side-effect of evaluating the
-	// getter for the property which is being replaced. This causes infinite
-	// recursion and an "Out of stack space" error.
-	let definingProperty = false;
-
-	return {
-		configurable: true,
-		get() {
-			// eslint-disable-next-line no-prototype-builtins
-			if (definingProperty || this === target.prototype || this.hasOwnProperty(key) ||
-        typeof _fn !== 'function') {
-				return _fn;
-			}
-
-			const boundFn = _fn.bind(this);
-			definingProperty = true;
-			Object.defineProperty(this, key, {
-				configurable: true,
-				get() {
-					return boundFn;
-				},
-				set(value) {
-					_fn = value;
-					delete this[key];
-				}
-			});
-			definingProperty = false;
-			return boundFn;
-		},
-		set(value: any) {
-			_fn = value;
-		}
-	};
 }
