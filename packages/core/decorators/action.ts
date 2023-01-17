2f292ffcb7c45da48edb5562e8b7e9d6a9ad5ac3
Samuel Bazaga <samuelbazaga@gmail.com>
2023-01-17 12:19:23 +0100

    Wrap action with class and add new methods

--- packages/core/decorators/action.ts
+++ packages/core/decorators/action.ts
@@ -1,7 +1,25 @@
 import { ActionMetadata } from "../interfaces/action-metadata";
 
 export function Action(actionMetadata: ActionMetadata) {
-  return function(target: any) {
-    target.selector = actionMetadata.selector;
+
+  if (!actionMetadata.selector) throw new Error('Action selector is required');
+
+  return function _ActionDecorator<T extends {new(...args: any[]): {}}>(constructor: T) {
+
+    (constructor as any).selector = actionMetadata.selector;
+    return class ActionClass extends constructor {
+
+      selector: string = actionMetadata.selector;
+
+      update(element: any, ...args: any[]) {
+        this['setup'](element, ...args);
+      }
+
+      clean() {
+        this['cleanup'] && this['cleanup']();
+      }
+
+    }
+
   }
 }
