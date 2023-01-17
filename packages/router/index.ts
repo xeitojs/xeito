8d36149b4a48eb8b246b65a93555d66230da4936
Samuel Bazaga <samuelbazaga@gmail.com>
2023-01-17 18:08:21 +0100

    fix(router): Add missing exports

--- packages/router/index.ts
+++ packages/router/index.ts
@@ -7,7 +7,11 @@ export { RouterSlot } from './components/router-slot';
 export { RouterLink } from './components/router-link';
 
 // Export the Xeito Router Interfaces
+export type { RouteParams } from './interfaces/route-params';
 export type { Route } from './interfaces/route';
 export type { RouterGuard } from './interfaces/router-guard';
 export type { RouterOptions } from './interfaces/router-options';
 export type { XeitoRouter } from './interfaces/xeito-router';
+
+// Re-exports from the History package
+export type { Update, Listener, Location } from 'history';
