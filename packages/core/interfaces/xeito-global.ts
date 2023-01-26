41aeca7711cfd50d34ecce3a4aac25189f9067bb
Samuel Bazaga <samuelbazaga@gmail.com>
2023-01-26 18:55:26 +0100

    fix(core): Wrong array type

--- packages/core/interfaces/xeito-global.ts
+++ packages/core/interfaces/xeito-global.ts
@@ -3,7 +3,7 @@ import { XeitoComponent } from "../classes/xeito-component";
 export interface XeitoGlobal {
 
   properties: Record<string, any>;
-  components: XeitoComponent[];
+  components: Array<typeof XeitoComponent>;
   pipes: any[];
   actions: any[];
 
