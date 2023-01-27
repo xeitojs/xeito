db796595a179c8d7bb42f2c857a67a71f70e7fbf
Samuel Bazaga <samuelbazaga@gmail.com>
2023-01-27 20:43:19 +0100

    Add support for shadowMode in component metadata
    (removed)

--- packages/core/interfaces/component-metadata.ts
+++ packages/core/interfaces/component-metadata.ts
@@ -1,9 +1,10 @@
 
 export interface ComponentMetadata {
-  selector: string;   // The selector of the component (e.g. 'app-root')
-  shadow?: boolean;   // If the component should use shadow dom
-  imports?: any[];    // Other components that should be imported to be used in the template (e.g. [CounterComponent])
-  actions?: any[];    // Actions that should be imported to be used in the template (e.g. [TextColorAction])
-  pipes?: any[];      // Pipes that should be imported to be used in the template (e.g. [UpperCasePipe])
-  services?: any[];   // Services that should be imported to be used in the template (e.g. [UserService]) Only to prevent minifiers from removing them
+  selector: string;                // The selector of the component (e.g. 'app-root')
+  shadow?: boolean;                // If the component should use shadow dom
+  shadowMode?: 'open' | 'closed';  // The shadow dom mode
+  imports?: any[];                 // Other components that should be imported to be used in the template (e.g. [CounterComponent])
+  actions?: any[];                 // Actions that should be imported to be used in the template (e.g. [TextColorAction])
+  pipes?: any[];                   // Pipes that should be imported to be used in the template (e.g. [UpperCasePipe])
+  services?: any[];                // Services that should be imported to be used in the template (e.g. [UserService]) Only to prevent minifiers from removing them
 }
