96de30bd0a9a5810e66430b75812b505e4f3b636
Samuel Bazaga <samuelbazaga@gmail.com>
2023-02-07 19:00:12 +0100

    Update examples

--- examples/basic/app-component.ts
+++ examples/basic/app-component.ts
@@ -1,6 +1,4 @@
 import { XeitoComponent, Component, html, State } from '../../packages/core';
-import { MixedStore } from '../../packages/store/classes/mixed-store';
-import { WriteStore } from '../../packages/store/classes/write-store';
 import { CounterComponent } from './counter-component';
 
 @Component({
@@ -11,41 +9,12 @@ import { CounterComponent } from './counter-component';
 })
 export class AppComponent extends XeitoComponent {
     
-  @State() counter = new WriteStore(0);
-  @State() derived = new MixedStore(this.counter, (counter) => counter * 2);
-
-  readCounter = new WriteStore(1);
-  readCounter2 = new WriteStore(0);
-
-  custom = new MixedStore([this.readCounter, this.readCounter2], ([one, two]) => {
-    return one + two;
-  });
-
-  sub = this.custom.subscribe((val) => console.log(val));
-
-  reset() {
-    this.readCounter2.set(this.readCounter2.value + 1)
-    this.counter.set(0);
-  }
-
-  increment() {
-    this.counter.update(n => n + 1);
-  }
-
-  decrement() {
-    this.counter.update(n => n - 1);
-  }
+  @State() count: number = 0;
 
   render() {
     return html`
       <h1>Hello World!</h1>
-      <h2>The count is: ${this.counter.value}</h2>
-      <p>Derived: ${this.derived.value}</p>
-      <div>
-        <button @click=${this.reset}>Reset</button>
-        <button @click=${this.decrement}>-</button>
-        <button @click=${this.increment}>+</button>
-      </div>
+      <button @click=${()=>this.count++}>Count is: ${this.count}</button>
     `;
   }
 
