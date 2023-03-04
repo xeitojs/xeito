3fd79a19c4cfd9dc37ecee0f705377ec1e71287c
Samuel Bazaga <samuelbazaga@gmail.com>
2023-03-04 02:36:40 +0100

    test(pipe): Improve tests for `@Pipe`

--- packages/core/__tests__/decorators/pipe.spec.ts
+++ packages/core/__tests__/decorators/pipe.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { Pipe } from "../../decorators/pipe";
 
 describe('@Pipe() decorator', () => {
@@ -7,10 +7,24 @@ describe('@Pipe() decorator', () => {
     expect(Pipe).toBeInstanceOf(Function);
   })
 
+  test('@Pipe() decorator throws an error if the selector is not defined', () => {
+    expect(() => Pipe({selector:null})(class {})).toThrowError();
+  })
+
   test('@Pipe() decorator returns a class with pipe methods', () => {
 
+    const transformFn = vi.fn();
+    const destroyFn = vi.fn();
     // Mock pipe
-    class TestP {}
+    class TestP {
+      transform(value) {
+        transformFn();
+        return value;
+      }
+      destroy() {
+        destroyFn();
+      }
+    }
 
     // Decorate the class
     const P = Pipe({selector: 'testP'})(TestP);
@@ -21,6 +35,13 @@ describe('@Pipe() decorator', () => {
     // Check if the instance has the pipe methods
     expect(p.update).toBeInstanceOf(Function);
     expect(p.clean).toBeInstanceOf(Function);
+    
+    expect(p.update('test'), null).toBe('test');
+    expect(transformFn).toHaveBeenCalled();
+
+    expect(p.clean()).toBeUndefined();
+    expect(destroyFn).toHaveBeenCalled();
+    
   })
 
 });
