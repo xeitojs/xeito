c7b32b72106bdd317e7d9292504627a66058e678
Samuel Bazaga <samuelbazaga@gmail.com>
2023-03-04 03:03:26 +0100

    test(xeito): Improve tests for Xeito base class

--- packages/core/__tests__/classes/xeito.spec.ts
+++ packages/core/__tests__/classes/xeito.spec.ts
@@ -1,5 +1,8 @@
+import { html } from 'uhtml';
 import { describe, expect, test } from 'vitest';
 import { Xeito } from '../../classes/xeito';
+import { XeitoComponent } from '../../classes/xeito-component';
+import { Component } from '../../decorators/component';
 
 describe('Xeito Application Class', () => {
  
@@ -17,6 +20,29 @@ describe('Xeito Application Class', () => {
     
     expect(xeito.bootstrap).toBeInstanceOf(Function);
     expect(xeito.usePlugin).toBeInstanceOf(Function);
+  });
+
+  test('Xeito class can be bootstrapped', () => {
+    
+    const root = document.createElement('div');
+    root.setAttribute('id', 'root');
+    document.body.appendChild(root);
+
+    // Mock component
+    class C extends XeitoComponent {
+      render() {
+        return html`<div>Test</div>`
+      }
+    }
+    Component({ selector: 'c-test' })(C);
+
+    const xeito = new Xeito(C);
+    xeito.bootstrap('#root');
+
+    const componentInstance = document.querySelector('#root').children[0];
+
+    expect(componentInstance).toBeInstanceOf(C);
+    expect(componentInstance.innerHTML).toBe('<div>Test</div>');
   });
 
 });
