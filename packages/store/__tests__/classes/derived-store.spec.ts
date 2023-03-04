108c925e110588145faa8b53e361aa8d5344c756
Samuel Bazaga <samuelbazaga@gmail.com>
2023-03-04 13:49:52 +0100

    test: Update tests for DerivedStore

--- packages/store/__tests__/classes/derived-store.spec.ts
+++ packages/store/__tests__/classes/derived-store.spec.ts
@@ -1,6 +1,7 @@
 import { describe, expect, test, vi } from "vitest";
 import { DerivedStore } from "../../classes/derived-store";
 import { ReadStore } from "../../classes/read-store";
+import { WriteStore } from "../../classes/write-store";
 
 describe('DerivedStore', () => {
 
@@ -13,7 +14,6 @@ describe('DerivedStore', () => {
     const store = new DerivedStore(rStore, ()=>{});
     expect(store).toHaveProperty('value');
     expect(store).toHaveProperty('subscribe');
-    expect(store).toHaveProperty('set');
   })
 
   test('DerivedStore should call the subscribe method of the dependency store', () => {
@@ -29,6 +29,58 @@ describe('DerivedStore', () => {
     const store = new DerivedStore(rStore, (value) => {
       expect(value).toBe(1);
     });
+  })
+
+  test('DerivedStore should emit the initial value upon subscription', () => {
+    const rStore = new ReadStore(1);
+    const store = new DerivedStore(rStore, (value) => {
+      return value;
+    });
+
+    const listener = vi.fn((value: number) => {
+      expect(value).toBe(1);
+    })
+
+    store.subscribe(listener);
+
+    expect(listener).toBeCalledTimes(1);
+  })
+
+  test('DerivedStore should call listeners when dependencies change', () => {
+    const wStore = new WriteStore(1);
+    const store = new DerivedStore(wStore, (value) => {
+      return value;
+    });
+
+    const listener = vi.fn();
+
+    store.subscribe(listener);
+
+    expect(listener).toBeCalledTimes(1);
+
+    wStore.set(2);
+
+    expect(listener).toBeCalledTimes(2);
+
+  })
+
+  test('DerivedStore subscription can be unsubscribed', () => {
+    const wStore = new WriteStore(1);
+    const store = new DerivedStore(wStore, (value) => {
+      return value;
+    });
+
+    const listener = vi.fn();
+
+    const subscription = store.subscribe(listener);
+
+    expect(listener).toBeCalledTimes(1);
+
+    subscription.unsubscribe();
+
+    wStore.set(2);
+
+    expect(listener).toBeCalledTimes(1);
   })
 
 })
