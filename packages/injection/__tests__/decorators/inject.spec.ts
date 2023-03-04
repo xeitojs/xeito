978dc0c26e32440c6066aca2545596f1f3d47526
Samuel Bazaga <samuelbazaga@gmail.com>
2023-03-04 13:49:35 +0100

    test: Improve test coverage for `@Inject`

--- packages/injection/__tests__/decorators/inject.spec.ts
+++ packages/injection/__tests__/decorators/inject.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { Inject } from "../../decorators/inject";
 import { Injectable } from "../../decorators/injectable";
 
@@ -50,6 +50,37 @@ describe('@Inject() decorator', () => {
     // Check if property is set
     expect(c2.invalidName).toBeInstanceOf(S2);
 
+  })
+
+  test('@Inject should warn if we try to set the value manually', () => {
+    class S3 {}
+
+    // Register mock service
+    Injectable({selector: 's3'})(S3);
+
+    // Create mock component
+    class C3 {
+      s3: S3;
+    }
+
+    // Decorate property with @Inject
+    Inject()(C3.prototype, 's3');
+
+    // Create instance of mock component
+    const c3 = new C3();
+
+    // Check if property is set
+    expect(c3.s3).toBeInstanceOf(S3);
+
+    const warn = vi.fn();
+    console.warn = warn;
+    // Try to set the value manually
+    c3.s3 = 'test';
+
+    // Check that the property is still set
+    expect(c3.s3).toBeInstanceOf(S3);
+    // Check that the warning was called
+    expect(warn).toBeCalledTimes(1);
   })
 
 })
