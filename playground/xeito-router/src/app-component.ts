6e76b7af19c700cbe06d98cf122224684d2728ff
Samuel Bazaga <samuelbazaga@gmail.com>
2023-03-09 22:45:26 +0100

    chore: Update playground

--- playground/xeito-router/src/app-component.ts
+++ playground/xeito-router/src/app-component.ts
@@ -1,23 +1,12 @@
-import { XeitoComponent, Component, html, State } from '../../../packages/core/index';
+import { XeitoComponent, Component, html } from '../../../packages/core/index';
 
 @Component({
   selector: 'app-root'
 })
 export class AppComponent extends XeitoComponent {
 
-  @State() show: boolean = true;
-
-  onWillMount() {
-    setInterval(() => {
-      this.show = !this.show;
-    }, 1000)
-  }
-
   render() {
     return html`
-      <xt-if .when=${this.show}>
-        <h1>App Component</h1>
-      </xt-if>
       <router-slot />
     `;
   }
